0ed6d90bf6041adcd302ce63c19c3526b9e04378
Dan Hauenstein <danhauen@gmail.com>
2020-02-04 16:38:38 -0600

    html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,14 @@
- // Firebase configuration
-//  var firebaseConfig = {
-//     apiKey: "AIzaSyBL6MdC8YSMWLRjAPp0XzbaU4sL1NF2T2g",
-//     authDomain: "train-time-748e6.firebaseapp.com",
-//     databaseURL: "https://train-time-748e6.firebaseio.com",
-//     projectId: "train-time-748e6",
-//     storageBucket: "train-time-748e6.appspot.com",
-//     messagingSenderId: "1032777039572",
-//     appId: "1:1032777039572:web:d23b7a5a1db733ac648541",
-//     measurementId: "G-ZTS4P9KVDN"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
+var firebaseConfig = {
 
-// // global variables
-// var database = firebase.database();
+};
 
-// Moment.js
-moment().format();
+firebase.initializeApp(firebaseConfig);
 
-// Current Time
-var timeCurrent = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+var database = firebase.database();
 
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-
-// display current time (Brandon showed me this)
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-  }
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
 
 // Button for adding train
@@ -51,27 +18,25 @@ $("#submit-train-btn").on("click", function (event) {
     // Grabs user input
     var trainName = $("#inputTrainName").val().trim();
     var trainDest = $("#inputDestination").val().trim();
-    var trainFirstTime = moment($("#inputFirstTrainTime").val().trim(), "HH:mm").format("X");
+    var trainFirst = moment($("#inputFirstTrainTime").val().trim(), "HH:mm").format("X");
     var trainFreq = $("#inputFrequency").val().trim();
 
     // Creates local "temporary" object for holding train data
     var newTrain = {
         name: trainName,
         destination: trainDest,
-        firstTrain: trainFirstTime,
+        firstTrain: trainFirst,
         frequency: trainFreq
     };
 
     // Uploads train data to the database
     database.ref().push(newTrain);
 
-    // console log newTrain info
     console.log(newTrain.name);
     console.log(newTrain.destination);
     console.log(newTrain.firstTrain);
     console.log(newTrain.frequency);
 
-    // lets user know train was added - would like to use a modal instead(maybe)
     alert("Train successfully added");
 
     // Clears all of the text-boxes
@@ -96,6 +61,10 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log(trainFreq);
     console.log(trainNextArrival);
 
+    // ?
+    var  = moment.unix(trainNextArrival).format("hh:mm");
+
+
     // Calculate the minutes away
     var trainMinAway = //
         console.log(trainMinAway);
@@ -111,5 +80,4 @@ database.ref().on("child_added", function (childSnapshot) {
     );
 
     // Append the new row to the table
-    $("#schedule-table > tbody").append(newRow); 
-});
+    $("#schedule-table > tbody").append(newRow);
